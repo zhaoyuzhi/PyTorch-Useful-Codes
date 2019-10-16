e80be49b2d261ebf66cc972f28a8795e43861e4c
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-10-16 16:02:08 +0800

    Add files via upload

--- PerceptualSimilarity/compute_dists_dirs.py
+++ PerceptualSimilarity/compute_dists_dirs.py
@@ -3,12 +3,12 @@ import os
 import models
 from util import util
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-d0','--dir0', type=str, default='./imgs/ex_dir0')
-parser.add_argument('-d1','--dir1', type=str, default='./imgs/ex_dir1')
-parser.add_argument('-o','--out', type=str, default='./imgs/example_dists.txt')
+parser.add_argument('-d0','--dir0', type=str, default='D:\\dataset\\Video\\test\\processed\\colorization\\ECCV16\\DAVIS-gray')
+parser.add_argument('-d1','--dir1', type=str, default='D:\\dataset\\Video\\test\\input\\DAVIS')
+parser.add_argument('-o','--out', type=str, default='./example_dists.txt')
 parser.add_argument('--use_gpu', action='store_true', help='turn on flag to use GPU')
 
 opt = parser.parse_args()
@@ -16,23 +16,36 @@ opt = parser.parse_args()
 ## Initializing the model
 model = models.PerceptualLoss(model='net-lin',net='alex',use_gpu=opt.use_gpu)
 
+# It is better to use this code on Windows 10 OS
+def get_files(path):
+    # Read a folder, return the complete path
+    ret = []
+    for root, dirs, files in os.walk(path):
+        for filespath in files:
+            if filespath[-3:] == 'jpg':
+                ret.append(os.path.join(root, filespath))
+    return ret
+imglist_dir0 = get_files(opt.dir0)
+imglist_dir1 = get_files(opt.dir1)
+assert len(imglist_dir0) == len(imglist_dir1)
+totallen = len(imglist_dir0)
+print(totallen)
+
 # crawl directories
 f = open(opt.out,'w')
-files = os.listdir(opt.dir0)
 
-for file in files:
-    if(os.path.exists(os.path.join(opt.dir1,file))):
-        # Load images
-        img0 = util.im2tensor(util.load_image(os.path.join(opt.dir0,file))) # RGB image from [-1,1]
-        img1 = util.im2tensor(util.load_image(os.path.join(opt.dir1,file)))
+for file in range(totallen):
+    # Load images
+    img0 = util.im2tensor(util.load_image(imglist_dir0[file])) # RGB image from [-1,1]
+    img1 = util.im2tensor(util.load_image(imglist_dir1[file]))
 
-        if(opt.use_gpu):
-            img0 = img0.cuda()
-            img1 = img1.cuda()
+    if(opt.use_gpu):
+        img0 = img0.cuda()
+        img1 = img1.cuda()
 
-        # Compute distance
-        dist01 = model.forward(img0,img1)
-        print('%s: %.3f'%(file,dist01))
-        f.writelines('%s: %.6f\n'%(file,dist01))
+    # Compute distance
+    dist01 = model.forward(img0,img1)
+    print('%s-th image: %.3f'%(file,dist01))
+    f.writelines('%s: %.6f\n'%(file,dist01))
 
 f.close()
