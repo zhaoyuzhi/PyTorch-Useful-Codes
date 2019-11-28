78c90e12e3037498ab166318292b545b8d9683d7
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-11-28 15:22:53 +0800

    Add files via upload

---
+++ mkdir.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 15:05:54 2019
+@author: zhaoyuzhi
+"""
+
+import os
+
+save_folder = './'
+
+if not os.path.exists(save_folder):
+    os.mkdir(save_folder)
