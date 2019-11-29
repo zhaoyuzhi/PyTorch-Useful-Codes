b25c7b5a9e35fcd884afdf20cd6e5fb425c8d5ea
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-11-29 10:48:19 +0800

    Add files via upload

---
+++ L2L1L0.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+# L2 loss --> expectation
+criterion_L2 = nn.MSELoss()
+
+# L1 loss --> medium
+criterion_L1 = nn.L1Loss()
+
+# L0 loss --> mode seeking (annealed version)
+class L0_Loss(nn.Module):
+    def __init__(self, total_epoch, reduction = 'mean'):
+        super(L0_Loss, self).__init__()
+        self.total_epoch = total_epoch
+        self.eps = 1e-8
+        self.reduction = reduction
+
+    def forward(self, x, y, current_epoch):
+        # power is annealed linearly from 2 to 0 during training; current_epoch is from 0 to (total_epoch - 1)
+        power = 2 * (self.total_epoch - current_epoch) / self.total_epoch
+        # base is the sum of difference and epsilon
+        base = torch.abs(x - y) + self.eps
+        # compute loss
+        ret = base ** power
+        if self.reduction != 'none':
+            ret = torch.mean(ret) if self.reduction == 'mean' else torch.sum(ret)
+
+        return ret
+
+criterion_L0 = L0_Loss(total_epoch = 100)           # the total_epoch should be pre-defined
