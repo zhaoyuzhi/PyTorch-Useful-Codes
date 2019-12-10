86cb6fe92b220b39db47b7df6171fa7a9f38f613
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-12-10 15:43:50 +0800

    Update network_module.py

--- network_module.py
+++ network_module.py
@@ -125,6 +125,85 @@ class LinearLayer(nn.Module):
             x = self.activation(x)
         return x
 
+#-----------------------------------------------
+#                Gated ConvBlock
+#-----------------------------------------------
+class GatedConv2dLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride = 1, padding = 0, dilation = 1, pad_type = 'reflect', activation = 'lrelu', norm = 'none', sn = False):
+        super(GatedConv2dLayer, self).__init__()
+        # Initialize the padding scheme
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+        
+        # Initialize the normalization type
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(out_channels)
+        elif norm == 'in':
+            self.norm = nn.InstanceNorm2d(out_channels)
+        elif norm == 'ln':
+            self.norm = LayerNorm(out_channels)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+        
+        # Initialize the activation funtion
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace = True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace = True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace = True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # Initialize the convolution layers
+        if sn:
+            self.conv2d = SpectralNorm(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation))
+            self.mask_conv2d = SpectralNorm(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation))
+        else:
+            self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation)
+            self.mask_conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding = 0, dilation = dilation)
+        self.sigmoid = torch.nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.pad(x)
+        conv = self.conv2d(x)
+        mask = self.mask_conv2d(x)
+        gated_mask = self.sigmoid(mask)
+        x = conv * gated_mask
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+
+class TransposeGatedConv2dLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride = 1, padding = 0, dilation = 1, pad_type = 'zero', activation = 'lrelu', norm = 'none', sn = True, scale_factor = 2):
+        super(TransposeGatedConv2dLayer, self).__init__()
+        # Initialize the conv scheme
+        self.scale_factor = scale_factor
+        self.gated_conv2d = GatedConv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation, norm, sn)
+    
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor = self.scale_factor, mode = 'nearest')
+        x = self.gated_conv2d(x)
+        return x
+
 # ----------------------------------------
 #            AdaIN Conv2d Block
 # ----------------------------------------
@@ -197,7 +276,7 @@ class ResConv2dLayer(nn.Module):
         # Initialize the conv scheme
         self.conv2d = nn.Sequential(
             Conv2dLayer(in_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation, norm, sn),
-            Conv2dLayer(in_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation = 'none', norm = norm, sn = sn)
+            Conv2dLayer(out_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation = 'none', norm = norm, sn = sn)
         )
         
         # Initialize the activation funtion
@@ -236,6 +315,41 @@ class ResAdaINConv2dLayer(nn.Module):
         residual = x
         out = self.conv2d(x, style)
         out = out + residual
+        return out
+
+class ResGatedConv2dLayer(nn.Module):
+    def __init__(self, channels, kernel_size = 3, stride = 1, padding = 1, dilation = 1, pad_type = 'zero', activation = 'lrelu', norm = 'none', sn = True):
+        super(ResGatedConv2dLayer, self).__init__()
+        # Initialize the conv scheme
+        self.conv2d = nn.Sequential(
+            GatedConv2dLayer(in_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation, norm, sn),
+            GatedConv2dLayer(out_channels, out_channels, kernel_size, stride, padding, dilation, pad_type, activation = 'none', norm = norm, sn = sn)
+        )
+        
+        # Initialize the activation funtion
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace = True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace = True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace = True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+    
+    def forward(self, x):
+        residual = x
+        out = self.conv2d(x)
+        out = out + residual
+        if self.activation:
+            out = self.activation(out)
         return out
 
 # ----------------------------------------
