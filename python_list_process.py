e1035835b64fba070b58e60712b4745ab862bf54
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2019-12-19 17:23:29 +0800

    Update python_list_process.py

--- python_list_process.py
+++ python_list_process.py
@@ -1,27 +1,35 @@
 import random
 
 # minus
+print('minus')
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 b = [2, 5, 7]
-
 c = [item for item in a if item not in b]
 print(c)
 
 # add
+print('add')
 a = [1, 2, 3]
 b = [3, 4, 5, 6]
-
 c = a + b
 print(c)
 
 # shuffle
+print('shuffle')
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 slice = random.sample(a, 5)
 print(slice)
 
 # shuffle and minus
+print('shuffle and minus')
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 slice = random.sample(a, 3)
 minus = [item for item in a if item not in slice]
 print(slice)
 print(minus)
+
+# shuffle the units in the list
+print('shuffle the units in the list')
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+random.shuffle(a)
+print(a)
