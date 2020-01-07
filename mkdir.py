a163e30701e9a0dfbb649f976f02745f42755ad2
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2020-01-07 15:01:45 +0800

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -11,7 +11,13 @@ save_folder = './'
 if not os.path.exists(save_folder):
     os.mkdir(save_folder)
 
+# single-layer folder
 def check_path(path):
     if not os.path.exists(path):
         os.mkdir(path)
+
+# multi-layer folder
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
         
