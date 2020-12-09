f07a146210984fab09c3a61814689ed4e46a4cc4
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2020-12-09 15:05:30 +0800

    Update TVLoss.py

--- TVLoss.py
+++ TVLoss.py
@@ -9,9 +9,9 @@ import torch
 import torch.nn as nn
 
 class TVLoss(nn.Module):
-    def __init__(self, TVLoss_weight = 1):
+    def __init__(self):
         super(TVLoss, self).__init__()
-        self.TVLoss_weight = TVLoss_weight
+        
     def forward(self, x):
         batch_size = x.size()[0]
         h_x = x.size()[2]
@@ -20,6 +20,7 @@ class TVLoss(nn.Module):
         count_w = self._tensor_size(x[:, :, :, 1:])
         h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
         w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
-        return self.TVLoss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+        return 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+    
     def _tensor_size(self, t):
         return t.size()[1] * t.size()[2] * t.size()[3]
