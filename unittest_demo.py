ac4710004a0f44fa4204056b306c3ab0dc9a476b
Yuzhi ZHAO <531708015@qq.com>
2024-03-21 16:36:03 +0800

    Create unittest_demo.py

---
+++ unittest_demo.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+#! coding=UTF-8
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    
+    # unittest.main()
+    #以上两句可以替换为下面两句
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
